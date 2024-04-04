e59390e1ff35dda0bb559f1df7c695851c26fc22
Piwong1 <142533789+Piwong1@users.noreply.github.com>
2024-04-04 12:20:58 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -37,7 +37,7 @@ public class Sound
    *  Precondition: samples contains at least one nonzero value
    *  Postcondition: the length of samples reflects the removal of starting silence
    */
-  public static void trimSilenceFromBeginning()
+  public void trimSilenceFromBeginning()
   {
     int start=0;
     boolean flip=false;
